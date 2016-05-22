a025af64344b62c8191760c46bd48f815d7407e5
ncardozo92 <ncardozo19@gmail.com>
2016-05-23 00:42:29 +0200

    Add files via upload
    commit del 22/05/2016

--- php/incluiBootstrap.php
+++ php/incluiBootstrap.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
 <meta charset="utf-8"/>
 <script src="jquery/jquery-1.12.3.js"></script>
 <script src="http://code.jquery.com/jquery.js"></script>
