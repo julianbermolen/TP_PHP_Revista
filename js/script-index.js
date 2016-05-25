b0a4ac114af30f1afbbf5dcb57b7da79eeb7df4d
ncardozo92 <ncardozo19@gmail.com>
2016-05-25 18:02:17 -0300

    Add files via upload
    añadido el script del menu en el index

---
+++ js/script-index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var contenedorDeDiarios=$("#contenedorDeDiarios");//escondo por defecto el contenedor de los diarios
+	var contenedorDeRevistas=$("#contenedorDeRevistas");
+	var tiposDeProductos= $("ul.navbar-nav > li");
+
+	$("li>a").click(function(event){
+		event.preventDefault();
+	});
+
+	contenedorDeDiarios.hide(0);
+
+	tiposDeProductos.eq(0).click(function(){
+		tiposDeProductos.removeClass("active");
+		$(this).addClass("active");
+		contenedorDeDiarios.hide(0);
+		contenedorDeRevistas.show(0);
+	});
+
+	tiposDeProductos.eq(1).click(function(){
+		tiposDeProductos.removeClass("active");
+		$(this).addClass("active");
+		contenedorDeDiarios.show(0);
+		contenedorDeRevistas.hide(0);
+	});
+
+
+
+});
