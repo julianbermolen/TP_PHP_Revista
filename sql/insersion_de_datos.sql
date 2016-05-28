c393b87286b98a6b9604e5efcb84251ff2e56c3c
ncardozo92 <ncardozo19@gmail.com>
2016-05-28 19:34:39 -0300

    Add files via upload
    Base de datos actualizada e inserción de datos en la tabla de publicación

---
+++ sql/insersion_de_datos.sql
@@ -0,0 +1,12 @@
+use sistema;
+
+insert into publicacion
+values(1,'La Naci&oacute;n','D','imagenes/portada_la_nacion.jpg'),
+(2,'Rolling Stones','R','imagenes/portada_rolling_stones.jpg'),
+(3,'Revista &Ntilde','R','imagenes/portada_enie.jpg');
+
+insert into publicacion
+values (4,'Clar&iacute;n','R','imagenes/portada_clarin.jpg');
+
+select *
+from publicacion;
