c5e5d779c5951248920823735b78be12c653fac5
julianbermolen <julianbermolen@gmail.com>
2016-06-14 13:17:59 -0300

    Base de datos con dato de prueba de articulo

--- sql/tp_php_revista_13_06.sql
+++ sql/tp_php_revista_13_06.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-06-2016 a las 01:18:01
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.21
+-- Tiempo de generación: 14-06-2016 a las 18:15:57
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.5.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -34,6 +34,13 @@ CREATE TABLE `articulo` (
   `id_seccion` int(11) DEFAULT NULL,
   `coordenadas` varchar(1024) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`id_articulo`, `titulo`, `subtitulo`, `texto`, `id_seccion`, `coordenadas`) VALUES
+(1, 'No se pone la gorra', 'Messi es furor en toda la Copa América Centenario', 'Lionel Messi genera revuelo en cualquier parte donde vaya, eso no es novedad. Y, por supuesto, Seattle no fue la excepción. La Selección Argentina se encuentra en esa ciudad para disputar el último partido del grupo D ante Bolivia, desde las 23 hs, y volvió a entrenarse con los juveniles del equipo local, en este caso el Seattle Sounders FC, algo que ya es costumbre en esta Copa América Centenario. La otra imagen habitual del seleccionado en el certamen es la de Leo posando con todo el mundo: esta vez, hasta un policía le pidió una foto.', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -103,6 +110,14 @@ CREATE TABLE `imagen` (
   `path` varchar(100) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id_imagen`, `id_articulo`, `path`) VALUES
+(1, 1, 'messi.jpg'),
+(2, 1, 'messi.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -128,6 +143,13 @@ CREATE TABLE `publicacion` (
   `descripcion` varchar(200) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `publicacion`
+--
+
+INSERT INTO `publicacion` (`id_publicacion`, `nombre_publicacion`, `tipo_publicacion`, `path`, `descripcion`) VALUES
+(1, 'Messi el futbol mundial', 'R', 'portada_rolling_stone.jpg', 'Messi: El mayor idolo del futbol argentino. Su historia de vida');
+
 -- --------------------------------------------------------
 
 --
@@ -150,6 +172,13 @@ CREATE TABLE `seccion` (
   `id_publicacion` int(11) NOT NULL,
   `nombre_sec` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `seccion`
+--
+
+INSERT INTO `seccion` (`id_seccion`, `id_publicacion`, `nombre_sec`) VALUES
+(1, 1, 'Seleccion');
 
 -- --------------------------------------------------------
 
@@ -263,7 +292,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `cliente`
 --
@@ -283,7 +312,7 @@ ALTER TABLE `edicion`
 -- AUTO_INCREMENT de la tabla `imagen`
 --
 ALTER TABLE `imagen`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `provincia`
 --
@@ -293,7 +322,7 @@ ALTER TABLE `provincia`
 -- AUTO_INCREMENT de la tabla `publicacion`
 --
 ALTER TABLE `publicacion`
-  MODIFY `id_publicacion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_publicacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
@@ -303,7 +332,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `seccion`
 --
 ALTER TABLE `seccion`
-  MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `suscripcion`
 --
