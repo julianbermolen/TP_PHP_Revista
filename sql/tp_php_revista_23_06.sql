e02dd77cbb52ecbb5ce658e951cd6db24587ab23
lauuteeh <lautaro.aragones@live.com.ar>
2016-06-23 13:44:44 -0300

    BD | Cambios en cliente + Ciudad
    Agregue ciudad, ademas de datos hardcodeados de provincia y ciudad
    (Organizado)

---
+++ sql/tp_php_revista_23_06.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-06-2016 a las 18:15:57
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.5.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+drop database if exists sistema;
+create database if not exists sistema;
+
+use sistema;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulo`
+--
+
+CREATE TABLE `articulo` (
+  `id_articulo` int(11) NOT NULL,
+  `titulo` varchar(75) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `subtitulo` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `texto` text CHARACTER SET latin1,
+  `id_seccion` int(11) DEFAULT NULL,
+  `coordenadas` varchar(1024) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`id_articulo`, `titulo`, `subtitulo`, `texto`, `id_seccion`, `coordenadas`) VALUES
+(1, 'No se pone la gorra', 'Messi es furor en toda la Copa América Centenario', 'Lionel Messi genera revuelo en cualquier parte donde vaya, eso no es novedad. Y, por supuesto, Seattle no fue la excepción. La Selección Argentina se encuentra en esa ciudad para disputar el último partido del grupo D ante Bolivia, desde las 23 hs, y volvió a entrenarse con los juveniles del equipo local, en este caso el Seattle Sounders FC, algo que ya es costumbre en esta Copa América Centenario. La otra imagen habitual del seleccionado en el certamen es la de Leo posando con todo el mundo: esta vez, hasta un policía le pidió una foto.', 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `email_cliente` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `clave_cliente` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `username_cliente` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `cod_rol` int(11) DEFAULT NULL,
+  `nombre_cliente` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido_cliente` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `calle` varchar(60) CHARACTER SET latin1 NOT NULL,
+  `numero_calle` varchar(6) CHARACTER SET latin1 NOT NULL,
+  `cod_prov` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `id_compra` int(11) NOT NULL,
+  `cod_edicion` int(11) DEFAULT NULL,
+  `cod_cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `edicion`
+--
+
+CREATE TABLE `edicion` (
+  `id_edicion` int(11) NOT NULL,
+  `id_publicacion` int(11) DEFAULT NULL,
+  `nombre_edicion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `cod_suscripcion` int(11) NOT NULL,
+  `precio_compra` decimal(4,2) DEFAULT NULL,
+  `precio_suscripcion` decimal(4,2) DEFAULT NULL,
+  `tapa` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL,
+  `descripcion` varchar(10) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id_imagen` int(11) NOT NULL,
+  `id_articulo` int(11) DEFAULT NULL,
+  `path` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id_imagen`, `id_articulo`, `path`) VALUES
+(1, 1, 'messi.jpg'),
+(2, 1, 'messi.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincia`
+--
+
+CREATE TABLE `provincia` (
+  `id_provincia` smallint(2) NOT NULL,
+  `provincia_nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE `ciudad` (
+  `id_ciudad` int(4) NOT NULL,
+  `ciudad_nombre` varchar(60) NOT NULL,
+  `cp` int(4) NOT NULL,
+  `cod_provincia` smallint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+
+--
+-- Estructura de tabla para la tabla `publicacion`
+--
+
+CREATE TABLE `publicacion` (
+  `id_publicacion` int(11) NOT NULL,
+  `nombre_publicacion` varchar(25) CHARACTER SET latin1 DEFAULT NULL,
+  `tipo_publicacion` char(1) CHARACTER SET latin1 DEFAULT NULL,
+  `path` varchar(300) CHARACTER SET latin1 DEFAULT NULL,
+  `descripcion` varchar(200) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `publicacion`
+--
+
+INSERT INTO `publicacion` (`id_publicacion`, `nombre_publicacion`, `tipo_publicacion`, `path`, `descripcion`) VALUES
+(1, 'Messi el futbol mundial', 'R', 'portada_rolling_stone.jpg', 'Messi: El mayor idolo del futbol argentino. Su historia de vida');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `descripcion` varchar(40) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seccion`
+--
+
+CREATE TABLE `seccion` (
+  `id_seccion` int(11) NOT NULL,
+  `id_publicacion` int(11) NOT NULL,
+  `nombre_sec` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `seccion`
+--
+
+INSERT INTO `seccion` (`id_seccion`, `id_publicacion`, `nombre_sec`) VALUES
+(1, 1, 'Seleccion');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `suscripcion`
+--
+
+CREATE TABLE `suscripcion` (
+  `id_suscripcion` int(11) NOT NULL,
+  `cod_cliente` int(11) DEFAULT NULL,
+  `cod_susc` int(11) NOT NULL,
+  `inicio` date NOT NULL,
+  `fin` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `email` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `clave` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `cod_rol` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`id_articulo`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`id_compra`);
+
+--
+-- Indices de la tabla `edicion`
+--
+ALTER TABLE `edicion`
+  ADD PRIMARY KEY (`id_edicion`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id_imagen`);
+
+--
+-- Indices de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD PRIMARY KEY (`id_provincia`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`id_ciudad`);
+
+--
+-- Indices de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD PRIMARY KEY (`id_publicacion`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  ADD PRIMARY KEY (`id_seccion`,`id_publicacion`);
+
+--
+-- Indices de la tabla `suscripcion`
+--
+ALTER TABLE `suscripcion`
+  ADD PRIMARY KEY (`id_suscripcion`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `edicion`
+--
+ALTER TABLE `edicion`
+  MODIFY `id_edicion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  MODIFY `id_publicacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `suscripcion`
+--
+ALTER TABLE `suscripcion`
+  MODIFY `id_suscripcion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
