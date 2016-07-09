3d75df3cfed64e1199be0b334ddf9a07fe3dcb80
lauuteeh <lautaro.aragones@live.com.ar>
2016-07-09 04:34:19 -0300

    Datos | usuario

---
+++ sql/Datos/Usuario/usuario.sql
@@ -0,0 +1,8 @@
+use sistema;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+INSERT INTO `usuario` (`id_usuario`, `email`, `clave`, `nombre`, `cod_rol`) VALUES
+(null, 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 'administrador', '3'),
+(null, 'conten@conten.com', '202cb962ac59075b964b07152d234b70', 'contenedista', '2'); 
